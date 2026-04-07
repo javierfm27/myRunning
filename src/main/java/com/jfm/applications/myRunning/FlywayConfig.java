61a037b939d819b1f1b41d4fa6b2df171f83b466
javierfm27 <fdezmorjavier@gmail.com>
2026-04-07 19:45:44 +0200

    Added flyway database migration

---
+++ src/main/java/com/jfm/applications/myRunning/FlywayConfig.java
@@ -0,0 +1,22 @@
+package com.jfm.applications.myRunning;
+
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    @DependsOn("dataSource")
+    public Flyway flyway(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
+}
